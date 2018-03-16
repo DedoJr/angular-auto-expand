7ce306fde42aaa5b2dbcd7702fa0c37e0411009b
Edison Jr <edison.honoriojr@outlook.com>
2018-03-16 15:31:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+(function(angular) {
+    'use strict';
+
+    angular
+        .module('alterdata.os')
+        .directive('autoExpand', autoExpand);
+
+    autoExpand.$inject = [];
+
+    function autoExpand() {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+
+                element.css({
+                    resize: 'none'
+                });
+
+                element.attr('rows', 1);
+
+                function update() {
+                    element.height('auto');
+                    var scrollHeight = element.get(0).scrollHeight;
+                    var paddingTop = parseInt(element.css('padding-top'))
+                    var paddingBottom = parseInt(element.css('padding-bottom'));
+                    var targetHeight = scrollHeight - (paddingTop + paddingBottom);
+                    element.height(targetHeight);
+                }
+
+                element.on('input', update);
+
+                update();
+            }
+        };
+    };
+})(window.angular);
